3ab15eaacc45e54b164685686189e9b2044e9e47
Androidzombie <andrewredondo4@gmail.com>
2017-06-01 05:13:51 -0400

    Optimized square function and cleaned code
    square function now takes about 0.014 seconds and is shorter.
    The square function is a bit off though and this will need to be fixed somehow.
    added plant_tracking_result.png and deadFunctions.py for any deadFunctions that we may want to recreate or upgrade in the future for reference.

--- test-Andrew.py
+++ test-Andrew.py
@@ -2,11 +2,8 @@ import cv2, time
 import numpy as np
 from matplotlib import pyplot as plt
 
-#Not used in this test (ROI - cropping)
-#rows,cols,channels = img.shape
-#plant1 = img[30:90, 0:70]
-#plant2 = img[0:100, 100:210]
-
+#Canny Edge detection with noise filtering.
+#(noise filtering currently commented out for testing on image with white background)
 def plantEdge(image):
     #Read image and make it gray
     #img = cv2.imread(image)
@@ -33,164 +30,77 @@ def plantEdge(image):
     #cv2.imshow("edges", edges)
     return edges
 
-
-
-def square(image):
-    y=0
+#This function creates the square around the plant and will also be used
+#to get the height/width of the plant
+def square(image,max_image_size):
+    t0=time.time()
     x=0
-    boundary = [100,100]
-    minmax = []
-    t0=int(time.time())
-    while y < boundary[1]-1:
-        print x,y
-        if (image[x,y]==255) and (not minmax):
-            minmax = [x,y,x,y]
-        elif (image[x,y]==255) and (minmax):
-            if minmax[0] > x:
-                minmax[0] = x
-            elif minmax[2] < x:
-                minmax[2] = x
-            if minmax[1] > y:
-                minmax[1] = y
-            elif minmax[3] < y:
-                minmax[3] = y
+    y=0
+    minmax = [0,0,0,0]
+    boundary = max_image_size - 1
+    while True: #(top)
+       #print x, y
+       if image[x,y]==255: minmax[1] = y; break
+       if(x < boundary) : x += 1
+       else: x=0; y+=1
 
-        if (x<boundary[0]-1):
-            x+=1
-        else:
-            x=0
-            y+=1
-    t1 = int((time.time()))
-    tf = t1 - t0
+    x=boundary
+    y=0
+    while True: #(right)
+        
+       if image[x,y]==255: minmax[2] = x; break
+       if(y < boundary) : y += 1
+       else: y=0; x-=1
+
+    x=0
+    y=boundary
+    while True: #(bottom)
+       if image[x,y]==255: minmax[3] = y; break
+       if(x < boundary) : x += 1
+       else: x=0; y-=1
+
+    x=0
+    y=0
+    while True: #(left)
+       if image[x,y]==255: minmax[0] = x; break
+       if(y < boundary) : y += 1
+       else: y=0; x+=1
+    t1= time.time()
+    tf=t1-t0
     cv2.rectangle(image, (minmax[0], minmax[1]), (minmax[2],minmax[3]), (255,0,0),2)
     return minmax, tf
-            
-def leftside(image):
-    y = 0
-    x = 0
-    D = []
 
-    while y<100:
-        if image[x,y] == 255:
-            #distance from left is x, add the distance to the list "D"
-            D.append(x) 
-            #move onto the next row
-            x=0
-            y+=1
-        elif (image[x,y] == 0) and (x>=50):
-            y+=1
-            x=0
-
-        x+=1
-
-        if x==100:
-            x=0
-            y+=1
-    minLeft = min(D)
-    return minLeft #return point closest to the left side
-
-def rightside(image):
-    y = 0
-    x = 99
-    D = []
-
-    while y<100:
-        if image[x,y] == 255:
-            d=100-x #Distance from right side
-            D.append(d)
-            #move onto the next row
-            x=99
-            y+=1
-        elif (image[x,y] == 0) and (x<=50):
-            y+=1
-            x=99
-
-        x-=1
-
-        if x==100:
-            x=99
-            y+=1
-    minRight = 100- min(D)
-
-    return minRight #return point closest to the right side
-
-def topside(image):
-    x = 0
-    y = 0
-    D = []
-
-    while x<100:
-        if image[x,y] == 255:
-            #distance from top is y, add the distance to the list "D"
-            D.append(y) 
-            #move onto the next row
-            y=0
-            x+=1
-        elif (image[x,y] == 0) and (y>=50):
-            x+=1
-            y=0
-
-        y+=1
-
-        if y==100:
-            y=0
-            x+=1
-    minTop = min(D)
-    return minTop #return point closest to the top side
-
-def botside(image):
-    x = 0
-    y = 99
-    D = []
-
-    while x<100:
-        if image[x,y] == 255:
-            d=100-y #Distance from bot side
-            D.append(d)
-            #move onto the next row
-            y=99
-            x+=1
-        elif (image[x,y] == 0) and (y<=50):
-            x+=1
-            y=99
-
-        y-=1
-
-        if y==100:
-            y=99
-            x+=1
-    minBot = 100- min(D)
-
-    return minBot #return point closest to the bot side
-
-
-    
-    
+#loads image into program
 img = cv2.imread('5plantDifferentS.png')
-edges = cv2.Canny(img,100,100)
+edges = cv2.Canny(img,100,100)  #Perform Canny Edge Detection
 
+#Crop each individual plant
 plant1 = edges[0:100, 0:100]
-print square(plant1)
-
-
-'''
 plant2 = edges[200:300, 0:100]
 plant3 = edges[100:200, 100:200]
 plant4 = edges[0:100, 200:300]
 plant5 = edges[200:300, 200:300]
-'''
-#edges = plantEdge(img)
 
+#Generate points for square and writes square to image.
+#Returns square coordinates and time taken to perform function
+print square(plant1, 100)
+print square(plant2, 100)
+print square(plant3, 100)
+print square(plant4, 100)
+print square(plant5, 100)
 
+#Draws images to the screen in seperate windows
 cv2.imshow("plant1", plant1)
-'''
 cv2.imshow("plant2", plant2)
-
 cv2.imshow("plant3", plant3)
 cv2.imshow("plant4", plant4)
 cv2.imshow("plant5", plant5)
-'''
-#Press Q to close all windows
+
+
+
+
+
+#Press Q to close all windows (Necessary or program throws error)
 if cv2.waitKey(1) & 0xFF == ord('q'):
     cv2.destroyAllWindows()
 cv2.waitKey(0)
