da9fc4ff869285d0432fa7761741b31f90f6a153
Androidzombie <andrewredondo4@gmail.com>
2017-06-04 16:23:52 -0400

    modified some code

--- test-Andrew.py
+++ test-Andrew.py
@@ -4,30 +4,23 @@ from matplotlib import pyplot as plt
 
 #Canny Edge detection with noise filtering.
 #(noise filtering currently commented out for testing on image with white background)
-def plantEdge(image):
+def plantEdge(image,noise_flag):
     #Read image and make it gray
-    #img = cv2.imread(image)
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    #cv2.imshow("gray", gray)
 
     #Apply a binary threshold
     ret,thresh1 = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
-    #cv2.imshow("thresh1", thresh1)
 
-    #apply opening morph to remove smaller noise
-    kernal2 = np.ones((6,6),np.uint8)
-    opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN,kernal2)
-    opening = cv2.dilate(opening, None)
-    #cv2.imshow("opening", opening)
-
+    if noise_flag == 1:
+        #apply opening morph to remove smaller noise
+        kernal2 = np.ones((6,6),np.uint8)
+        opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN,kernal2)
+        opening = cv2.dilate(opening, None)
+        edges = cv2.Canny(opening,100,100)
+        return edges
+        
     #Detect edges from the final morphed img
-    edges = cv2.Canny(opening,100,100)
-    #Trying to add the overlay sides to the original image
-    #dst = cv2.addWeighted(img,0.7,edges,0.3,0)
-    #cv2.imshow("image", image)
-
-    #ret,dst = cv2.threshold(edges,127,255,cv2.THRESH_BINARY_INV)
-    #cv2.imshow("edges", edges)
+    edges = cv2.Canny(thresh1,100,100)
     return edges
 
 #This function creates the square around the plant and will also be used
@@ -65,10 +58,17 @@ def square(image,max_image_size):
        if image[x,y]==255: minmax[0] = x; break
        if(y < boundary) : y += 1
        else: y=0; x+=1
+
+    #100px = 100mm so answer is in mm (recalculate for actual physicial implementations
+    plant_width = minmax[2] - minmax[0]
+    plant_length = minmax[3] - minmax[1]
+    
     t1= time.time()
     tf=t1-t0
     cv2.rectangle(image, (minmax[0], minmax[1]), (minmax[2],minmax[3]), (255,0,0),2)
-    return minmax, tf
+
+    #return minmax, tf
+    return plant_width, plant_length
 
 #loads image into program
 img = cv2.imread('5plantDifferentS.png')
@@ -95,9 +95,6 @@ cv2.imshow("plant2", plant2)
 cv2.imshow("plant3", plant3)
 cv2.imshow("plant4", plant4)
 cv2.imshow("plant5", plant5)
-
-
-
 
 
 #Press Q to close all windows (Necessary or program throws error)
